43bb34761551fb029a0057834a7d5b0f8710747d
matt silverstein <welcomematt@gmail.com>
2013-09-17 01:11:57 -0700

    moved state logic from controllers into game service

--- www/js/controllers/questionCtrl.js
+++ www/js/controllers/questionCtrl.js
@@ -1,47 +1,27 @@
 'use strict';
 
-function QuestionCtrl($scope,$rootScope,socket,game) {
+function QuestionCtrl($scope,game) {
+
+  $scope.game = game;
 
   $scope.answerSubmitted = false;
-  $scope.cycleTime = null;
-  $scope.cycleTimeRemaining = null;
   $scope.questionBarVal = 100;
 
   //message can change depending on game state
   $scope.progressBarMsg = "Submit your answer before time's up!";
   //update progress bar
-  $scope.$watch('cycleTimeRemaining', function() {
-    $scope.questionBarVal = $scope.cycleTimeRemaining / $scope.cycleTime * 100;
-  });
-
-  //listen for question events
-  socket.on('newQuestion', function(data) {
-    $rootScope.question.text = data.question;
-    $scope.cycleTime = data.cycleTime;
-    $scope.answerSubmitted = false;
-  });
-
-  socket.on('cycleUpdate', function(data) {
-    if (data.gameState === 'question') {
-      $scope.cycleTime = data.cycleTime;
-      $scope.cycleTimeRemaining = data.cycleTimeRemaining;
-    }
+  $scope.$watch('game.cycleTime', function() {
+    $scope.questionBarVal = $scope.game.cycleTime / $scope.game.cycleTotalTime * 100;
   });
 
   $scope.submitAnswer = function() {
     var answerObj = {
       answer: $scope.answer,
-      username: $rootScope.username
     };
-    socket.emit('submitAnswer', answerObj);
+    game.submitAnswer(answerObj);
+
     $scope.answerform.$setPristine();
     $scope.answerSubmitted = true;
-    console.log('submitted Answer!');
   };
-
-  //destroy listeners when scope is destroyed to prevent duplicate listeners
-  $scope.$on('$destroy', function (event) {
-    socket.removeAllListeners();
-  });
 
 }
--- www/js/services/gameSvc.js
+++ www/js/services/gameSvc.js
@@ -1,6 +1,29 @@
 myApp.factory('game', function ($rootScope, socket) {
+  var game = {};
+  game.gameState = null;
+  game.question = null;
+  game.cycleTime = null;
+  game.cycleTotalTime = null;
 
-  return {
+  socket.on('cycleUpdate', function(data) {
+    if (game.gameState !== data.gameState) {
+      game.gameState = data.gameState;
+      game.cycleTotalTime = data.cycleTotalTime;
+    }
+    if (data.question && game.question !== data.question) {
+      game.question = data.question;
+    }
+    //updata cycleTime every update
+    game.cycleTime = data.cycleTime;
+    console.log(game.gameState);
+  });
 
+  socket.on('newQuestion', function(data) {
+  });
+
+  game.submitAnswer = function(answerObj) {
+    socket.emit('submitAnswer', answerObj);
   };
+
+  return game;
 });
