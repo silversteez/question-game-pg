231dd037ce3f53ec705806b96f801e0e2a3b2df6
matt silverstein <welcomematt@gmail.com>
2013-09-23 11:06:11 -0700

    putting scope vars on an object (so child scope can reference rather than copy)

--- www/js/controllers/questionCtrl.js
+++ www/js/controllers/questionCtrl.js
@@ -4,10 +4,12 @@ function QuestionCtrl($scope,game,navSvc) {
 
   $scope.game = game;
 
-  $scope.canSubmitAnswer = true;
-  $scope.answerSubmitted = false;
-  $scope.transToAnswer = false;
-  $scope.displayResults = false;
+  $scope.data = {};
+
+  $scope.data.canSubmitAnswer = true;
+  $scope.data.answerSubmitted = false;
+  $scope.data.transToAnswer = false;
+  $scope.data.answer = null;
 
   $scope.progressBarVal = 100;
   //message can change depending on game state
@@ -21,34 +23,30 @@ function QuestionCtrl($scope,game,navSvc) {
 
   $scope.$watch('game.gameState', function() {
     if ($scope.game.gameState === 'transition') {
-      $scope.canSubmitAnswer = false;
-      $scope.answerSubmitted = false;
-      $scope.transToAnswer = true;
+      $scope.data.canSubmitAnswer = false;
+      $scope.data.answerSubmitted = false;
+      $scope.data.transToAnswer = true;
     } else if ($scope.game.gameState === 'answer') {
       $scope.progressBarMsg = "Time until next question...";
-      $scope.transToAnswer = false;
-      $scope.canSubmitAnswer = false;
-      $scope.answerSubmitted = false;
-      $scope.displayResults = true;
+      $scope.data.transToAnswer = false;
+      $scope.data.canSubmitAnswer = false;
+      $scope.data.answerSubmitted = false;
     } else { //in question state
-      $scope.transToAnswer = false;
-      $scope.answerSubmitted = false;
-      $scope.canSubmitAnswer = true;
+      $scope.data.transToAnswer = false;
+      $scope.data.answerSubmitted = false;
+      $scope.data.canSubmitAnswer = true;
       $scope.progressBarMsg = "Submit your answer before time's up!";
     }
   });
 
   $scope.submitAnswer = function() {
+    console.log('scope is ', $scope);
     var answerObj = {
-      answer: $scope.answer,
+      answer: $scope.data.answer,
     };
     game.submitAnswer(answerObj);
-    $scope.answerform.$setPristine();
-    $scope.canSubmitAnswer = false;
-    $scope.answerSubmitted = true;
-  };
-
-  $scope.dismissResults = function() {
-    $scope.displayResults = false;
+    //$scope.data.answerform.$setPristine();
+    $scope.data.canSubmitAnswer = false;
+    $scope.data.answerSubmitted = true;
   };
 }
