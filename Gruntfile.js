d46852f23bf2cefb80e85c3d9f22ae47d0c8ab44
matt silverstein <welcomematt@gmail.com>
2013-09-24 15:31:42 -0700

    cleaning things up and trying to get grunt to compile stylus

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,35 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    stylus: {
+      compile: {
+        // options: {
+        //   'paths': [
+        //       // 'node_modules/',    // nib
+        //       'styl/'             // Individual components
+        //   ]
+        // },
+        files: {
+          './css/styles.css': ['./styl/styles.styl']
+        }
+      }
+    },
     copy: {
       main: {
         src: 'www/**',
         dest: '../question-game-be/',
       },
     },
+    watch: {
+
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['copy']);
-
+  grunt.registerTask('default', ['stylus', 'copy']);
+  grunt.registerTask('justCopy', ['copy']);
 };
