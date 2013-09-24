85e65dcbb0c0ef23fffc08b816aab56041fc57a5
matt silverstein <welcomematt@gmail.com>
2013-09-23 19:20:29 -0700

    setting up package.json and grunt for copying www files to server dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,14 @@ module.exports = function(grunt) {
   grunt.initConfig({
     copy: {
       main: {
-        src: 'www/*',
-        dest: 'test/',
+        src: 'www/**',
+        dest: '../question-game-be/',
       },
     },
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default', ['copy']);
 
 };
